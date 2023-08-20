09a26f88c63ab150acc143449677b41151e25e13
Ícaro Chagas <icaro.chagas.a@gmail.com>
2023-08-19 22:02:56 -0300

    add test to createTask method

--- taskManager/src/test/java/TaskManagerTest.java
+++ taskManager/src/test/java/TaskManagerTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 public class TaskManagerTest {
 
@@ -9,5 +12,21 @@ public class TaskManagerTest {
     public void setUp() {
         taskRepository = new InMemoryTaskRepository();
         taskManager = new TaskManager(taskRepository);
+    }
+
+    @Test
+    public void testCreateTask() {
+
+        Task task = taskManager.createTask("Task 1", "Description 1", LocalDate.now(), Priority.HIGH);
+        assertNotNull(task);
+        assertTrue(task.getId() > 0);
+
+        // Verify that the task is in the repository
+        Task retrievedTask = taskRepository.findById(task.getId());
+        assertNotNull(retrievedTask);
+        assertEquals("Task 1", retrievedTask.getTitle());
+        assertEquals("Description 1", retrievedTask.getDescription());
+        assertEquals(LocalDate.now(), retrievedTask.getDueDate());
+        assertEquals(Priority.HIGH, retrievedTask.getPriority());
     }
 }
